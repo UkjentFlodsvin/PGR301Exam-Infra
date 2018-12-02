8a2fa7dd87788501f2eb406af26d72fc9d86e5aa
UkjentFlodsvin <nylmar15@student.westerdals.no>
2018-12-02 22:19:10 +0100

    Trying to make tfvars work 12

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,9 +3,3 @@ variable "app_prefix" {default = "book-api"}
 variable "pipeline_name" {default = "book-api-pipeline"}
 
 variable "heroku_provider" {}
-
-resource "null_resource" "action1" {
-  provisioner "local-exec" {
-    command = "echo ${var.heroku_provider}"
-  }
-}
