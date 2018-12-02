74a48337a0b89c6ff5bd31051a4a8384315c8dd1
UkjentFlodsvin <nylmar15@student.westerdals.no>
2018-12-02 14:34:41 +0100

    typo in variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "app_prefix" {default = "book-api"}
 
-variable "pipeline_name" {default = "book.api-pipeline"}
+variable "pipeline_name" {default = "book-api-pipeline"}
 
 variable "heroku_provider" {default = "nylmar15@student.kristiania.no"}
